936be5990b91c26699415c18b28bd71e56d0541b
Kirubapriya R <kiru2632@global.chainsys.com>
2021-06-08 11:35:02 +0530

    f1

--- src/test/java/in/kiruba/service/TestService.java
+++ src/test/java/in/kiruba/service/TestService.java
@@ -8,16 +8,16 @@ public class TestService {
 	@Test
 	public void testService() {
 
-		String name = "Livith";
+		String name = "Kavitha";
 		String password = "Livith@15";
 
-		String email = "livith11@gmail.com";
+		String email = "Kavith11@gmail.com";
 
-		long mobileNo = 9999998880L;
+		long mobileNo = 9991998880L;
 
 		int age = 41;
 
-		long adhaarNo = 344578903257L;
+		long adhaarNo = 340578903257L;
 
 		boolean valid = RegisterServiceValidation.checkUserDetail(name, password, email, age, mobileNo, adhaarNo);
 
