17f1ca13edccba473441e03dd4a307d9a70d8b3e
Kirubapriya R <kiru2632@global.chainsys.com>
2021-06-08 12:10:08 +0530

    f1:register

--- src/test/java/in/kiruba/service/TestService.java
+++ src/test/java/in/kiruba/service/TestService.java
@@ -8,42 +8,143 @@ public class TestService {
 	@Test
 	public void testService() {
 
-		String name = "Jeeva";
-		String password = "Jeeva@123";
+		String name = "Kaviya";
+		String key = "Kavi@123";
 
-		String email = "pavi2000@gmail.com";
+		String email = "kavi17@gmail.com";
 
-		long mobileNo = 9999908880L;
+		long mobileNo = 9909908080L;
 
 		int age = 41;
 
-		long adhaarNo = 342578903257L;
+		long adhaarNo = 542578903257L;
 
-		boolean valid = RegisterServiceValidation.checkUserDetail(name, password, email, age, mobileNo, adhaarNo);
+		boolean valid = RegisterServiceValidation.checkUserDetail(name, key, email, age, mobileNo, adhaarNo);
 
 		
 		assertTrue(valid);
 
 	}
 	@Test
-	public void testServiceWithIncorrectDetails() {
+	public void testServiceWithIncorrectUserName() {
 
 		String name = "livith";
-		String password = "Livith15";
+		String key = "Livith15";
 
-		String email = "livith@gmail.com";
+		String email = "livith20@gmail.com";
 
-		long mobileNo = 999999880L;
+		long mobileNo = 9999998809L;
 
-		int age = 0;
+		int age = 23;
 
-		long adhaarNo = 34457890357L;
+		long adhaarNo = 344578903571L;
 
-		boolean valid = RegisterServiceValidation.checkUserDetail(name, password, email, age, mobileNo, adhaarNo);
+		boolean valid = RegisterServiceValidation.checkUserDetail(name, key, email, age, mobileNo, adhaarNo);
 
 		
 		assertFalse(valid);
 
 	}
+	@Test
+	public void testServiceWithIncorrectUserPssword() {
+
+		String name = "Livith";
+		String key = "livith15";
+
+		String email = "livith20@gmail.com";
+
+		long mobileNo = 9999998802L;
+
+		int age = 87;
+
+		long adhaarNo = 344578190357L;
+
+		boolean valid = RegisterServiceValidation.checkUserDetail(name, key, email, age, mobileNo, adhaarNo);
+
+		
+		assertFalse(valid);
+
+	}
+	@Test
+	public void testServiceWithIncorrectEmail() {
+
+		String name = "Livith";
+		String key = "Livith@15";
+
+		String email = "livith1gmail.com";
+
+		long mobileNo = 9999998802L;
+
+		int age = 56;
+
+		long adhaarNo = 344578190357L;
+
+		boolean valid = RegisterServiceValidation.checkUserDetail(name, key, email, age, mobileNo, adhaarNo);
+
+		
+		assertFalse(valid);
+
+	}
+	@Test
+	public void testServiceWithIncorrectAge() {
+
+		String name = "Livith";
+		String key = "Livith@15";
+
+		String email = "livith20@gmail.com";
+
+		long mobileNo = 9999998802L;
+
+		int age = 0;
+
+		long adhaarNo = 344578190357L;
+
+		boolean valid = RegisterServiceValidation.checkUserDetail(name, key, email, age, mobileNo, adhaarNo);
+
+		
+		assertFalse(valid);
+
+	}
+	@Test
+	public void testServiceWithIncorrectMobileNumber() {
+
+		String name = "Livith";
+		String key = "Livith@15";
+
+		String email = "livith20@gmail.com";
+
+		long mobileNo = 999999802L;
+
+		int age = 34;
+
+		long adhaarNo = 344578190357L;
+
+		boolean valid = RegisterServiceValidation.checkUserDetail(name, key, email, age, mobileNo, adhaarNo);
+
+		
+		assertFalse(valid);
+
+	}
+	@Test
+	public void testServiceWithIncorrectAdhaarNumber() {
+
+		String name = "Livith";
+		String key = "Livith@15";
+
+		String email = "livith20@gmail.com";
+
+		long mobileNo = 9999998802L;
+
+		int age = 54;
+
+		long adhaarNo = 34457890357L;
+
+		boolean valid = RegisterServiceValidation.checkUserDetail(name, key, email, age, mobileNo, adhaarNo);
+
+		
+		assertFalse(valid);
+
+	}
+
 
 }
